737f6a02bccb64bcbef8f50087913e09f5640393
Mikko Möttönen <mottonen.mikko@outlook.com>
2026-04-14 23:49:19 +0300

    Build Finnish page in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        fi: resolve(__dirname, 'fi/index.html'),
+      },
+    },
+  },
 })
